42ab865175badddec573994a93d79bd2cd1f54a9
Nitiraj0611 <2020umt1352@mnit.ac.in>
2023-06-19 12:21:59 +0530

    model trainer commit

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -93,4 +93,3 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e, sys)
-
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.metrics import r2_score, mean_absolute_error
 
 from src.execption import CustomException
 
@@ -16,6 +17,32 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_r2_score = r2_score(y_train, y_train_pred)
+            test_r2_score = r2_score(y_test, y_test_pred)
+            test_mae = mean_absolute_error(y_test, y_test_pred)
+
+            report[model_name] = {
+                'R2 Score (Train)': train_r2_score,
+                'R2 Score (Test)': test_r2_score,
+                'MAE (Test)': test_mae
+            }
+
+        return report
+
 
     except Exception as e:
         raise CustomException(e, sys)
